bb38ef3605828d2ba3bff86913fac4957fedfafd
Antonio <66217078+Antonio-Dante@users.noreply.github.com>
2026-02-23 21:56:06 -0600

    Aplicacion en JavaScript
    Este programa muestra los datos del usuario y realiza operaciones basicas

---
+++ JavaScript/inicio.js
@@ -0,0 +1,26 @@
+let nombre = "Marco Antonio";
+let edad = 21;
+let carrera = "ICO";
+let correo = "marcoantonio@example.com";
+
+console.log("Nombre: " + nombre);
+console.log("Edad: " + edad);
+console.log("Carrera: " + carrera);
+console.log("Correo: " + correo);
+
+// Operaciones matematicas
+
+let num1 = 10;
+let num2 = 5;
+
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
+let modulo = num1 % num2;
+
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicación: " + multiplicacion);
+console.log("División: " + division);
+console.log("Módulo: " + modulo);
