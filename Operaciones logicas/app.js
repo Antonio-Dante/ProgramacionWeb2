e917a6177778cbdc8f61d2c4bed1c42d2bee3765
Antonio <66217078+Antonio-Dante@users.noreply.github.com>
2026-03-03 17:40:51 -0600

    Operaciones logicas
    Tarea con uso de operaciones logicas

---
+++ Operaciones logicas/app.js
@@ -0,0 +1,12 @@
+// Operadores logicos
+/*
+&&: Si los dos son verdaderos devuelve verdadero
+||: Con que uno sea verdadero da verdadero
+! : negacion
+*/
+//             false     true
+var compara = 10 < 8 && 10 == 10 ;
+console.log(compara);
+
+var compara = true && true;
+console.log(compara);
