a948c7bdfcb3894cbfc0311e3a9080726d3e297f
Antonio <66217078+Antonio-Dante@users.noreply.github.com>
2026-03-03 17:41:36 -0600

    Estructura if
    Uso de if como estructura de control

---
+++ Estructura if/app.js
@@ -0,0 +1,9 @@
+let mensaje = prompt("Ingresa tu contraseña");
+//    true 
+if(mensaje === "123456"){
+    console.log("Correcto :)");
+}else{
+    console.log("Error de contraseña :(");
+}
+
+console.log("Fin del programa ......")
